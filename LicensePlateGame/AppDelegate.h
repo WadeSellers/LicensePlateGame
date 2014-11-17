88d40c7821c91adc304b4f9964301c4aae69d23c
Wade Sellers <Waderoni@gmail.com>
2014-11-16 23:15:48 -0600

    Initial Commit

---
+++ LicensePlateGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LicensePlateGame
+//
+//  Created by Wade Sellers on 11/16/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LicensePlateGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LicensePlateGame
+//
+//  Created by Wade Sellers on 11/16/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
